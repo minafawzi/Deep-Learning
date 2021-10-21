acf2b3e65973d599a28f7ea2cd80321680d54517
Mina Hanna <minafawzi@gmail.com>
2021-10-20 21:47:20 -0500

    update

--- Tensorflow_Basic/MLP/1_Simple_f_approx/exercise_f_approx3d.py
+++ Tensorflow_Basic/MLP/1_Simple_f_approx/exercise_f_approx3d.py
@@ -1,3 +1,96 @@
+# %% --------------------------------------- Imports -------------------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import Adam
+
+
+# %% ----------------------------------- Hyper Parameters --------------------------------------------------------------
+# It is always good practice to set the hyper-parameters at the beginning of the script
+# And even better to define a params class if the script is long and complex
+LR = 0.05
+N_NEURONS = 100
+N_EPOCHS = 10000
+
+# %% -------------------------------------- Data Prep ------------------------------------------------------------------
+# Gets the input interval and the targets
+x1 = np.linspace(-3, 3, 500)
+x2 = np.linspace(-3, 3, 500)
+
+# reshape the inputs to work with the MLP
+p= np.column_stack((x1,x2))
+#print(p)
+
+#Define the function
+t = x1*x1 - x2*x2
+
+# %% -------------------------------------- Training Prep --------------------------------------------------------------
+# Defines the model
+
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape=(2,)))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Prepares a Stochastic Gradient Descent optimizer and a Mean Squared Error performance index
+#model.compile(optimizer=Adam(), loss="mean_squared_error")
+model.compile(optimizer=SGD(lr=LR), loss="mean_squared_error")
+
+# %% -------------------------------------- Training Loop --------------------------------------------------------------
+# Trains the model. We use full Batch GD.
+train_hist = model.fit(p, t, epochs=N_EPOCHS, batch_size=len(p))
+
+# %% --------------------------------------  meshgrid reshaping ---------------------------------------------------------------
+# reate meshgrid on the original function
+X1, X2 = np.meshgrid(x1, x2)
+T = X1*X1 - X2*X2
+
+#print(T.shape)
+#reshape the meshgrid to fit NN
+X3 = X1.reshape(-1,1)
+X4 = X2.reshape(-1,1)
+
+#Reshap the input by stacking the two X3 and X4 columns
+P= np.column_stack((X3,X4))
+
+#
+Output = model.predict(P)
+Output2 = Output.reshape(T.shape)
+#print(Output.shape)
+#print(X3.shape)
+#print(P.shape)
+plt.title("MLP fit to x1^2-x2^2 | MSE: {:.5f}".format(train_hist.history["loss"][-1]))
+##
+#print("X1:")
+#print(X1)
+#print("X2:")
+#print(X2)
+#print("T:")
+#print(T)
+
+#print("X3: ")
+#print(X3)
+#print("X4: ")
+#print(X4)
+#print("P: ")
+#print(P)
+#print("Output: ")
+#print(Output)
+#print("Output2: ")
+#print(Output2)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X1, X2, Output2, 50, cmap='Reds')
+ax.contour3D(X1, X2, T, 50, cmap='Wistia')
+ax.set_xlabel('x1')
+ax.set_ylabel('x2')
+ax.set_zlabel('y');
+plt.legend()
+plt.show()
+
 # %% -------------------------------------------------------------------------------------------------------------------
 
 # -------------------------------------
